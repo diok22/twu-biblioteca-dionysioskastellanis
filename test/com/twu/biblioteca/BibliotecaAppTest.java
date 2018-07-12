290a3cf7f3879a74c1ddbb60d3d8be3ecd7ea87c
diok22 <Dionysis.k22@gmail.com>
2018-07-12 18:34:20 +0100

    takes biblioteca variable to a before statement

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,14 +1,30 @@
 package com.twu.biblioteca;
 
-
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+
 public class BibliotecaAppTest {
+
+    public BibliotecaApp biblioteca;
+
+
+    @Before
+    public void setUp() {
+        biblioteca = new BibliotecaApp();
+    }
 
     @Test
     public void testWelcomeMessage() {
-        BibliotecaApp biblioteca = new BibliotecaApp();
         assertEquals("Welcome to our Biblioteca", biblioteca.welcomeMessage());
     }
+
+    /**
+    @Test
+    public void testListBooks() {
+        String books[] = {"Ready Player one", "The Alchemist"};
+        assertEquals( books, biblioteca.books());
+    }
+    */
 }
