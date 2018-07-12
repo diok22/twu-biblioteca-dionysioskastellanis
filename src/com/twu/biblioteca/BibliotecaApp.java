b293baa6e2870330d0c4a2cf9aea3518d2a3c282
diok22 <Dionysis.k22@gmail.com>
2018-07-12 17:32:50 +0100

    welcome message by default on, not only on test case

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,7 +3,8 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        
+        BibliotecaApp biblioteca = new BibliotecaApp();
+        biblioteca.welcomeMessage();
 
     }
 
