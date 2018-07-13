6ef203e6e0e77d49df0cfdc7fbdc0034500b6db1
diok22 <Dionysis.k22@gmail.com>
2018-07-13 15:08:10 +0100

    lists books with details, test fails

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -10,9 +10,9 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         BibliotecaApp biblioteca = new BibliotecaApp();
         biblioteca.welcomeMessage();
-        biblioteca.addBook("Head First Java", "Kathy Sierra & Bert Bates", 2005);
+        biblioteca.addBook("Ready Player 1", "Ernest Cline", 2011);
+        biblioteca.addBook("The Alchemist", "Paolo Coelho", 1993);
         biblioteca.listBooks();
-
 
     }
 
@@ -20,12 +20,17 @@ public class BibliotecaApp {
         return "Welcome to our Biblioteca";
     }
 
+
     public void addBook(String title, String author, int publicationDate) {
         Book addingBook = new Book(title, author, publicationDate);
         libraryBooks.add(addingBook);
     }
 
-    public String listBooks() {
-        return libraryBooks.get(0).title;
+    public void listBooks() {
+        System.out.println("Title of Book" + "\t\t" + "Author" + "\t\t" + "Publication Date");
+        for (Book book : libraryBooks) {
+            System.out.println(book.title + "\t\t" + book.author + "\t\t" + book.publicationDate);
+        }
     }
+
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -2,7 +2,7 @@ package com.twu.biblioteca;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class BibliotecaAppTest {
@@ -12,6 +12,8 @@ public class BibliotecaAppTest {
     @Before
     public void setUp() {
         biblioteca = new BibliotecaApp();
+        biblioteca.addBook("Ready Player 1", "Ernest Cline", 2011);
+        biblioteca.addBook("The Alchemist", "Paolo Coelho", 1993);
     }
 
     @Test
@@ -19,9 +21,19 @@ public class BibliotecaAppTest {
         assertEquals("Welcome to our Biblioteca", biblioteca.welcomeMessage());
     }
 
+
+    @Test
+    public void testAddBook() {
+        assertFalse(String.valueOf(false), biblioteca.libraryBooks.isEmpty());
+    }
+
+    /**
     @Test
     public void testListBooks() {
-        String books[] = {"Ready Player one", "The Alchemist"};
-        assertEquals(books, biblioteca.listBooks());
+        assertThat("Head First Java", biblioteca.listBooks());
     }
+    */
+
+
+
 }
